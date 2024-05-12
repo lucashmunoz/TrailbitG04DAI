1c8ff0a0b60eee44220e12b43b6614a907e3554d
lucashmunoz <lucashmunoz@gmail.com>
2024-05-12 03:02:12 -0300

    default eslint to ecma latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   root: true,
   plugins: ["prettier"],
-  extends: ["prettier"]
+  extends: ["prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  env: {
+    es6: true
+  }
 };
