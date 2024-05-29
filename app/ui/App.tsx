b4606a48896a3d414517903ee7ed1ef2716c1ef0
Ignacio Cesarani <84433278+icesarani@users.noreply.github.com>
2024-05-29 15:25:32 -0300

    Feature splash (#5)
    * implement splash screen
    
    * fix added files
    
    ---------
    
    Co-authored-by: lucashmunoz <lucashmunoz@gmail.com>
    Co-authored-by: Ignacio Cesarani <icesarani@MacBook-Air-de-Ignacio.local>

--- app/ui/App.tsx
+++ app/ui/App.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
 import RootNavigator from "../navigation/RootNavigator";
 import { Provider } from "react-redux";
 import store from "../state/store";
+import SplashScreen from 'react-native-splash-screen'
+import {Platform} from 'react-native'
+import BootSplash from 'react-native-bootsplash';
 
 function App(): React.JSX.Element {
+
+  useEffect(() => {
+    hideSplashScreen();
+  }, []);
+
+  const hideSplashScreen = async () => {
+    try {
+      if (Platform.OS === 'android') {
+        await BootSplash.hide({ fade: true });
+      } else {
+        SplashScreen.hide();
+      }
+    } catch (error) {
+      console.error('Error hiding splash screen', error);
+    }
+  };
+  
   return (
     <Provider store={store}>
       <RootNavigator />
