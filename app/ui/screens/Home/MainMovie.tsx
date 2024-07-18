1245b9c28ef33225379bdb6412831b13dff739c7
Ale Susmelj <alesusmelj06@gmail.com>
2024-07-18 01:48:42 -0300

    feat: Traibit logo with box shadow

--- app/ui/screens/Home/MainMovie.tsx
+++ app/ui/screens/Home/MainMovie.tsx
@@ -7,12 +7,23 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import NavigatorConstant from "../../../navigation/NavigatorConstant";
 import SkeletonPlaceholder from "react-native-skeleton-placeholder";
 import colors from "../../styles/colors";
+import { IMAGES } from "../../../assets/images/index";
+import LinearGradient from "react-native-linear-gradient";
 
 const MainMovie = () => {
   const dispatch = useAppDispatch();
   const { mainMovie, loading } = useAppSelector(state => state.mainMovie);
 
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  const gradientStart = { x: 0.5, y: 0 };
+  const gradientEnd = { x: 0.5, y: 1 };
+
+  const gradientColors = [
+    "rgba(0, 0, 0, 0.0)",
+    "rgba(0, 0, 0, 0.5)",
+    "rgba(0, 0, 0, 0.0)"
+  ];
 
   useEffect(() => {
     dispatch(fetchMainMovie());
@@ -43,13 +54,27 @@ const MainMovie = () => {
       </View>
     );
   }
-
+  //
   return (
     <View style={styles.principalMovieContainer}>
       <Image
         source={{ uri: mainMovie.images[0]?.file_path }}
         style={styles.backgroundImage}
       />
+
+      <LinearGradient
+        start={gradientStart}
+        end={gradientEnd}
+        colors={gradientColors}
+        style={styles.logoContainer}>
+        <View>
+          <Image
+            source={IMAGES.LOGO}
+            resizeMode="cover"
+            style={styles.logoImage}
+          />
+        </View>
+      </LinearGradient>
 
       <View style={styles.mainMovieContainer}>
         <TouchableOpacity
@@ -78,6 +103,20 @@ const styles = StyleSheet.create({
   backgroundImage: {
     height: 300,
     aspectRatio: 16 / 9
+  },
+  logoContainer: {
+    position: "absolute",
+    width: "100%",
+    height: 160,
+    overflow: "hidden",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1,
+    top: 60
+  },
+  logoImage: {
+    height: 50,
+    aspectRatio: 2911 / 859
   },
   mainMovieContainer: {
     position: "absolute",
