063dea29b17a3f158f4763512d785c548efc87e3
Ale Susmelj <alesusmelj06@gmail.com>
2024-07-17 21:51:05 -0300

    feat: fetch recent movies

--- app/ui/screens/Home/RecentMovies.tsx
+++ app/ui/screens/Home/RecentMovies.tsx
@@ -1,89 +1,40 @@
 import React, { useEffect } from "react";
-import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { useAppDispatch, useAppSelector } from "../../../state/store";
 import { fetchGenres } from "../../../state/slices/home/asyncThunks";
 import MovieCard from "./MovieCard";
 import colors from "../../styles/colors";
 import { FlashList } from "@shopify/flash-list";
 import GenreButtons from "./GenreButtons";
-
-const movies = [
-  {
-    id: "1",
-    title: "Movie 1",
-    image:
-      "https://res.cloudinary.com/dtrj85lgu/image/upload/v1720543446/uk9isj3wa4yhe98ucv8q.jpg"
-  },
-  {
-    id: "2",
-    title: "Movie 2",
-    image:
-      "https://res.cloudinary.com/dtrj85lgu/image/upload/v1720543446/uk9isj3wa4yhe98ucv8q.jpg"
-  },
-  {
-    id: "3",
-    title: "Movie 3",
-    image:
-      "https://res.cloudinary.com/dtrj85lgu/image/upload/v1720543446/uk9isj3wa4yhe98ucv8q.jpg"
-  },
-  {
-    id: "4",
-    title: "Movie 4",
-    image:
-      "https://res.cloudinary.com/dtrj85lgu/image/upload/v1720543446/uk9isj3wa4yhe98ucv8q.jpg"
-  },
-  {
-    id: "5",
-    title: "Movie 5",
-    image:
-      "https://res.cloudinary.com/dtrj85lgu/image/upload/v1720543446/uk9isj3wa4yhe98ucv8q.jpg"
-  },
-  {
-    id: "6",
-    title: "Movie 6",
-    image:
-      "https://res.cloudinary.com/dtrj85lgu/image/upload/v1720543446/uk9isj3wa4yhe98ucv8q.jpg"
-  },
-  {
-    id: "7",
-    title: "Movie 7",
-    image:
-      "https://res.cloudinary.com/dtrj85lgu/image/upload/v1720543446/uk9isj3wa4yhe98ucv8q.jpg"
-  },
-  {
-    id: "8",
-    title: "Movie 8",
-    image:
-      "https://res.cloudinary.com/dtrj85lgu/image/upload/v1720543446/uk9isj3wa4yhe98ucv8q.jpg"
-  }
-];
+import { fetchRecentMovies } from "../../../state/slices/recentMovies/asyncThunks";
 
 const RecentMovies = (): React.JSX.Element => {
   const dispatch = useAppDispatch();
-  const { genres, loading, error } = useAppSelector(state => state.home);
+  const { genres } = useAppSelector(state => state.home);
+  const { recentMovies } = useAppSelector(state => state.recentMovies);
 
   useEffect(() => {
-    dispatch(fetchGenres());
+    dispatch(fetchGenres()), dispatch(fetchRecentMovies({ page: 1 }));
   }, [dispatch]);
 
-  if (loading) {
-    return <ActivityIndicator size="large" color="#0000ff" />;
-  }
-
-  if (error) {
-    return <Text style={styles.errorText}>Error: {error}</Text>;
-  }
-
   const handleScroll = ({ nativeEvent }) => {};
+
+  const mapData = () => {
+    return recentMovies.map(movie => ({
+      ...movie,
+      id: movie.id.toString(),
+      image: movie.posterPath
+    }));
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.recentsTitle}>Recientes</Text>
       <GenreButtons genres={genres} />
       <FlashList
-        data={movies}
+        data={mapData()}
         renderItem={({ item }) => <MovieCard item={item} />}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item.id.toString()}
         numColumns={3}
         estimatedItemSize={120}
         onMomentumScrollEnd={handleScroll}
